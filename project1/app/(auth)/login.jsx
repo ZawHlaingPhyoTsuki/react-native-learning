21bed93aedf331364af4f81197cd21b0cdc1012b
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-12-25 21:50:08 +0700

    feat: add `ThemedButton` component and integrate it into login and register forms

--- project1/app/(auth)/login.jsx
+++ project1/app/(auth)/login.jsx
@@ -4,6 +4,7 @@ import { Link } from 'expo-router'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
     return (
@@ -14,6 +15,10 @@ const Login = () => {
             <ThemedText title style={styles.title} >
                 Login to Your Account
             </ThemedText>
+
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{ color: '#f2f2f2' }}>Login</Text>
+            </ThemedButton>
 
             <Spacer height={100} />
 
--- project1/app/(auth)/register.jsx
+++ project1/app/(auth)/register.jsx
@@ -4,6 +4,7 @@ import { Link } from 'expo-router'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Register = () => {
     return (
@@ -14,6 +15,10 @@ const Register = () => {
             <ThemedText title style={styles.title} >
                 Register for an Account
             </ThemedText>
+
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{ color: '#f2f2f2' }}>Register</Text>
+            </ThemedButton>
 
             <Spacer height={100} />
 
